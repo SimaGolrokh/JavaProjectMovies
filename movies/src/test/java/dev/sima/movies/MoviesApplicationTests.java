b37586c0dfbc615ceaedacf22744df420c773eb6
sima golrokh bahoush <sima.golrokh@gmail.com>
2024-07-18 08:11:32 +0200

    DB
    connecting mongodb to project

---
+++ movies/src/test/java/dev/sima/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sima.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
